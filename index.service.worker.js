51ad19e6528ce4eb0c2132582d70aee3f8f4cbe3
Afonso Tavares <afonsotavares@Afonsos-MacBook-Air.local>
2026-01-11 13:29:22 +0100

    Audio: Fix iOS Safari - JavaScript AudioContext unlock, proper stereo format, retry logic

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767963782|1737641';
+const CACHE_VERSION = '1768134549|1474166';
 /** @type {string} */
 const CACHE_PREFIX = 'Tretris-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
